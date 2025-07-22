b2ffd8c64cb1d330aeeffb1935d1dac90f71ec6c
Breanna B. <88126555+bb-codes6@users.noreply.github.com>
2025-07-22 13:08:34 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
                 </div>
                 <div className="overflow-hidden" style={{maxHeight: "30vh"}}>
                     <div className="container px-5">
-                        <img src="/georgie-cobbs-muOHbrFGEQY-unsplash.jpg" alt={"Hero-image"} className="img-fluid border rounded-3 shadow-lg mb-4" width={"700"} height={"500"} loading="lazy"/>
+                        <img src="Rare-App/georgie-cobbs-muOHbrFGEQY-unsplash.jpg" alt={"Hero-image"} className="img-fluid border rounded-3 shadow-lg mb-4" width={"700"} height={"500"} loading="lazy"/>
                     </div>
                 </div>
             </div>
